538fe57752c61688e96f2fb6fcab72c8477cf342
Payman <payman.zab@gmail.com>
2016-04-25 16:58:13 -0400

    j'ai rajouté les fichiers du prof

--- app/src/main/java/ca/ulaval/mp/mp/MainActivity.java
+++ app/src/main/java/ca/ulaval/mp/mp/MainActivity.java
@@ -5,9 +5,25 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String lat;
+    private String lon;
+    private String apiURL;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lat =getLat();
+        lon = getLon();//faire les get pour le deux surment devoir créé une class possition/géo
+        apiURL = "https://simple-weather.p.mashape.com/weather?lat="+lat+"&lng="+lon
+
+        HttpResponse<String> response = Unirest.get(apiURL) //faudrait implanté la library Unirest, voir http://unirest.io/java
+                .header("X-Mashape-Key", "xn55jeUBg0mshwmZSIrB2dNbudabp1xomN8jsnXryHN4ARtYyo")
+                .header("Accept", "text/plain")
+                .asString();
     }
+
+
 }
---
+++ app/src/main/java/ca/ulaval/mp/mp/util/Util.java
@@ -0,0 +1,23 @@
+package ca.ulaval.mp.mp.util;
+
+import android.content.Context;
+
+import java.util.Map;
+
+import ca.ulaval.mp.mp.R;
+
+public class Util {
+	public static String getFormatedAPIURL(Context inContext,String inMethod,Map<String, String> inParams){
+		String API_BASE = inContext.getResources().getString(R.string.API_BASE);
+		String API_format = inContext.getResources().getString(R.string.API_FORMAT);
+		if(inParams==null){
+			return API_BASE+API_format+inMethod;
+		}else{
+			return API_BASE+API_format+inMethod;
+		}
+	}
+	public static String getFormatedAPIURL(Context inContext,String inMethod){
+		return Util.getFormatedAPIURL(inContext, inMethod,null);
+	}
+
+}
